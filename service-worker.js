9192299aedc2683dbee3fdc5e318286327e980ba
Jreed212 <jeremy@thatguyssecret.com>
2026-08-30 16:18:23 -0500

    Refresh cache for group hub

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='strength-cycle-v17';
+const CACHE='strength-cycle-v18';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
