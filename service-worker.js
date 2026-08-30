fdd03654d110133d69cc1e105ea72f146f6d04db
Jreed212 <jeremy@thatguyssecret.com>
2026-08-30 17:38:45 -0500

    Refresh cache for atomic group creation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='strength-cycle-v19';
+const CACHE='strength-cycle-v20';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
