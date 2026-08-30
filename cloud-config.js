334aa9c43a8f2874d6107777c19d35fe7ad14736
Jreed212 <jeremy@thatguyssecret.com>
2026-08-30 14:46:06 -0500

    Add cloud backend config stub

---
+++ cloud-config.js
@@ -0,0 +1,6 @@
+// Public Supabase client configuration.
+// The anon key is designed to be public; Row Level Security protects user data.
+window.CLOUD_CONFIG = {
+  supabaseUrl: '',
+  supabaseAnonKey: ''
+};
