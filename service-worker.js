45dcdf3b8f869b1c2f9f81e3b9d4a434f5bfc411
Jreed212 <jeremy@thatguyssecret.com>
2026-08-30 13:08:38 -0500

    Refresh offline cache for Strength Cycle v8

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,8 @@
-const CACHE='strength-cycle-live-v1';
+const CACHE='strength-cycle-v8';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./',copy));return r}).catch(()=>caches.match('./')));return}
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
